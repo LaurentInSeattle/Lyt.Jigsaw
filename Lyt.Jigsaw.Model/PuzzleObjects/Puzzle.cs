3cc2ec0395b8cd19fc488fdb56cde8d7784e9d52
Laurent <ly.testud@outlook.com>
2025-10-10 17:05:56 -0700

    Some additional game properties and methods

--- Lyt.Jigsaw.Model/PuzzleObjects/Puzzle.cs
+++ Lyt.Jigsaw.Model/PuzzleObjects/Puzzle.cs
@@ -2,13 +2,53 @@
 
 public sealed class Puzzle
 {
-    public int PieceOverlap { get; set; }
+    private readonly Dictionary<int, Tuple<int, int>> pieceArrays; 
 
-    public int RotationStep { get; set; }
+    public Puzzle(int height, int width, int rotationStep)
+    {
+        this.Height = height;
+        this.Width = width;
+        this.RotationStep = rotationStep;
+        this.pieceArrays = [];
+        this.GeneratePieceArrays(); 
+    }
 
-    public int PieceCount { get; set; }
+    public int Height { get; private set; }
 
-    public List<Group> Groups { get; set; } = []; 
+    public int Width { get; private set; }
 
-    public bool IsComplete { get; }
+    public int Rows { get; private set; }
+
+    public int Columns { get; private set; }
+
+    public int PieceOverlap { get; private set; }
+
+    public int RotationStep { get; private set; }
+
+    public int PieceCount { get; private set; }
+
+    public List<Group> Groups { get; private set; } = [];
+
+    public bool IsComplete 
+        => this.Groups.Count == 1 && this.Groups[0].Pieces.Count == this.PieceCount;
+
+    public List<int> PieceCounts => [.. this.pieceArrays.Keys];
+
+    public bool SelectPieceCount (int pieceCount)
+    {
+        if (this.pieceArrays.TryGetValue(pieceCount, out var pieceArray) && pieceArray is not null)
+        {
+            this.Rows = pieceArray.Item1;
+            this.Columns = pieceArray.Item2;
+            this.PieceCount = pieceCount;
+            return true;
+        }
+
+        return false;
+    }
+
+    private void GeneratePieceArrays()
+    {
+
+    }
 }
